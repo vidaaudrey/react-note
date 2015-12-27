68f18dfda3c42431018fb21c31a3476dbcb9104e
vidaaudrey <audrey@33road.com>
2015-12-26 22:58:13 -0800

    feat: add reverse order for the display of notes

--- config.js
+++
@@ -1,3 +0,0 @@
-export default = {
-    githubAccessToken: '3ecbda7d88feda4cb16434fb2321ac36baef4fb3'
-}
--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -8,7 +8,7 @@ const Notes = ({username, notes, addNote}) => {
         < div >
             < h3 > Notes for {username} < /h3> 
             <AddNote username={username} addNote={addNote} />
-            < NotesList notes = {notes} />
+            < NotesList notes = {notes.reverse()} />
         < /div>
     )
     
